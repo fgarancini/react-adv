d5a7f6103ade35a6acd1b19b5a9b8a14707c3985
fgarancini <f.garancini15@gmail.com>
2023-06-25 18:25:45 -0300

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.tsx
+++ src/02-component-patterns/hooks/useShoppingCart.tsx
@@ -14,35 +14,35 @@ const useShoppingCart = () => {
     product: Product;
   }) => {
     setShoppingCart((oldShopingCart) => {
-      const productInCart: ProductInCard = oldShopingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShopingCart,
-          [product.id]: productInCart,
-        };
-      }
-
-      const { [product.id]: toDelete, ...rest } = oldShopingCart;
-      return rest;
-      // const newCart = {
-      //   ...oldShopingCart,
-      //   [product.id]: { ...product, count },
+      // const productInCart: ProductInCard = oldShopingCart[product.id] || {
+      //   ...product,
+      //   count: 0,
       // };
+
+      // if (Math.max(productInCart.count + count, 0) > 0) {
+      //   productInCart.count += count;
+      //   return {
+      //     ...oldShopingCart,
+      //     [product.id]: productInCart,
+      //   };
+      // }
+
+      // const { [product.id]: toDelete, ...rest } = oldShopingCart;
+      // return rest;
+      const newCart = {
+        ...oldShopingCart,
+        [product.id]: { ...product, count },
+      };
 
       // if (count === 0) {
       //   const { [product.id]: toDelete, ...rest } = oldShopingCart;
       //   return rest;
       // }
 
-      // if (count === 0) {
-      //   delete newCart[product.id];
-      // }
-      // return newCart;
+      if (count === 0) {
+        delete newCart[product.id];
+      }
+      return newCart;
     });
   };
 
